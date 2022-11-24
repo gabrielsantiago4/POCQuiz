bbe525d9dc5ba4e7c42a594a79d58593bcc077e3
Paulo Henrique <phsilva098@gmail.com>
2022-11-24 10:49:40 -0300

    Inital commit

---
+++ POCQuiz/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  POCQuiz
+//
+//  Created by Paulo Henrique Gomes da Silva on 24/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
