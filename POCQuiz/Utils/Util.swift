f52a1e80eb5c4f4492fdcc6be676915f95d3a0a9
Gabriel Santiago <gaaasts@gmail.com>
2022-11-30 09:39:11 -0300

    atualizado

--- POCQuiz/Utils/Util.swift
+++ POCQuiz/Utils/Util.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-func make<T: UIView>(_ uiView: T, _ configure: (T) -> Void) -> T {
+public func make<T: UIView>(_ uiView: T, _ configure: (T) -> Void) -> T {
     configure(uiView)
     return uiView
 }
